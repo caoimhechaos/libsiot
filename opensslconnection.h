3cc87e029b6ec4d14e35f133ccab8e785b84dbdd
Tonnerre LOMBARD <tonnerre@ancient-solutions.com>
2013-03-19 23:48:54 +0100

    Add an OpenSSL socket implementation.

---
+++ opensslconnection.h
@@ -0,0 +1,80 @@
+/*-
+ * Copyright (c) 2013 Tonnerre Lombard <tonnerre@ancient-solutions.com>,
+ *                    Ancient Solutions. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions  of source code must retain  the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions  in   binary  form  must   reproduce  the  above
+ *    copyright  notice, this  list  of conditions  and the  following
+ *    disclaimer in the  documentation and/or other materials provided
+ *    with the distribution.
+ *
+ * THIS  SOFTWARE IS  PROVIDED BY  ANCIENT SOLUTIONS  AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO,  THE IMPLIED WARRANTIES OF  MERCHANTABILITY AND FITNESS
+ * FOR A  PARTICULAR PURPOSE  ARE DISCLAIMED.  IN  NO EVENT  SHALL THE
+ * FOUNDATION  OR CONTRIBUTORS  BE  LIABLE FOR  ANY DIRECT,  INDIRECT,
+ * INCIDENTAL,   SPECIAL,    EXEMPLARY,   OR   CONSEQUENTIAL   DAMAGES
+ * (INCLUDING, BUT NOT LIMITED  TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE,  DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT  LIABILITY,  OR  TORT  (INCLUDING NEGLIGENCE  OR  OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <string>
+
+#include <openssl/ssl.h>
+
+#include "siot/server.h"
+#include "siot/ssl.h"
+#include "unixsocketconnection.h"
+
+struct sockaddr_storage;
+
+namespace toolbox
+{
+namespace siot
+{
+namespace ssl
+{
+class OpenSSLConfig
+{
+public:
+	OpenSSLConfig();
+};
+
+// This class represents a connection using OpenSSL for encryption.
+class OpenSSLConnection : public UNIXSocketConnection
+{
+public:
+	// Create a new OpenSSL server connection from the client connection
+	// "socketid", connected to "peer", and kick off negociation with
+	// the settings specified in "context".
+	OpenSSLConnection(Server* srv, int socketid,
+		       	struct sockaddr_storage* peer,
+		       	ServerSSLContext* context);
+	virtual ~OpenSSLConnection();
+
+	// Implements Connection.
+	virtual string Receive(size_t maxlen = -1, int flags = 0);
+	virtual ssize_t Send(string data, int flags = 0);
+	virtual uint64_t GetLastUse();
+	virtual void SetBlocking(bool blocking = true);
+	virtual void Shutdown();
+
+private:
+	const OpenSSLConfig& openssl_cfg_;
+	bool blocking_;
+	uint64_t last_use_;
+	SSL_CTX* ssl_ctx_;
+	SSL* ssl_handle_;
+};
+}  // namespace ssl
+}  // namespace siot
+}  // namespace toolbox
---
+++ opensslconnection_test.cc
@@ -0,0 +1,106 @@
+/**
+ * Tests for the OpenSSL socket connection implementation.
+ */
+
+#include <gtest/gtest.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <thread++/threadpool.h>
+#include <toolbox/qsingleton.h>
+
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+#include <iostream>
+
+#include "opensslconnection.h"
+
+namespace toolbox
+{
+namespace siot
+{
+namespace ssl
+{
+namespace testing
+{
+// using threadpp::ClosureThread;
+using threadpp::Thread;
+
+class OpenSSLConnectionSetup : public Thread
+{
+public:
+	OpenSSLConnectionSetup(SSL* ssl)
+	: ssl_(ssl)
+	{
+	}
+
+	virtual void Run()
+	{
+		unsigned long err = SSL_connect(ssl_);
+		if (err <= 0)
+		{
+			std::cerr << ERR_error_string(err, NULL) << std::endl;
+			throw ServerSetupException(
+					ERR_error_string(err, NULL));
+		}
+	}
+
+private:
+	SSL* ssl_;
+};
+
+class OpenSSLConnectionTest : public ::testing::Test
+{
+};
+
+TEST_F(OpenSSLConnectionTest, ReadWrite)
+{
+	ServerSSLContext* ctx = new ServerSSLContext("test.crt", "test.key");
+	const SSL_METHOD* meth = SSLv23_client_method();
+	struct sockaddr_storage ssladdr;
+	SSL_CTX* ssl_ctx;
+	SSL* ssl;
+	char buf[16];
+	int socks[2];
+
+	memset(&ssladdr, 0, sizeof(struct sockaddr_storage));
+	memset(buf, 0, sizeof(buf));
+
+	EXPECT_FALSE(socketpair(AF_UNIX, SOCK_STREAM, 0, socks))
+		<< "Error establishing socket pair: " << strerror(errno);
+
+	// We need to use OpenSSL, so make sure it's initialized.
+	QSingleton<OpenSSLConfig>::GetInstance();
+
+	ASSERT_NE((SSL_CTX*) 0, ssl_ctx = SSL_CTX_new(meth))
+		<< ERR_error_string(ERR_get_error(), NULL);
+	SSL_CTX_set_verify(ssl_ctx, SSL_VERIFY_NONE, NULL);
+	ASSERT_NE((SSL*) 0, ssl = SSL_new(ssl_ctx))
+		<< ERR_error_string(ERR_get_error(), NULL);
+	ASSERT_EQ(1, SSL_set_fd(ssl, socks[1]))
+		<< ERR_error_string(ERR_get_error(), NULL);
+
+	OpenSSLConnectionSetup setup(ssl);
+	setup.Start();
+
+	OpenSSLConnection sslc(0, socks[0], &ssladdr, ctx);
+	setup.WaitForFinished();
+
+	EXPECT_EQ(11, sslc.Send("Hey, buddy!"));
+	EXPECT_EQ(11, SSL_read(ssl, buf, sizeof(buf)));
+	EXPECT_EQ("Hey, buddy!", string(buf));
+
+	EXPECT_EQ(strlen(buf), SSL_write(ssl, buf, strlen(buf)));
+	EXPECT_EQ("Hey, buddy!", sslc.Receive());
+
+	SSL_shutdown(ssl);
+	SSL_free(ssl);
+	SSL_CTX_free(ssl_ctx);
+}
+
+}  // namespace testing
+}  // namespace ssl
+}  // namespace siot
+}  // namespace toolbox
