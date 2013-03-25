2c0b159275143aa06d6022ccc410fcdd28531924
Caoimhe Chaos <caoimhechaos@protonmail.com>
2013-03-26 00:40:23 +0100

    Add a decorator for a socket which will return the same data over and over again until it has been acknowledged.

---
+++ acknowledgementdecorator.cc
@@ -0,0 +1,120 @@
+/*-
+ * Copyright (c) 2013 Caoimhe Chaos <caoimhechaos@protonmail.com>,
+ *                    Ancient Solutions. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions  of source code must retain  the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions  in   binary  form  must   reproduce  the  above
+ *    copyright  notice, this  list  of conditions  and the  following
+ *    disclaimer in the  documentation and/or other materials provided
+ *    with the distribution.
+ *
+ * THIS  SOFTWARE IS  PROVIDED BY  ANCIENT SOLUTIONS  AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO,  THE IMPLIED WARRANTIES OF  MERCHANTABILITY AND FITNESS
+ * FOR A  PARTICULAR PURPOSE  ARE DISCLAIMED.  IN  NO EVENT  SHALL THE
+ * FOUNDATION  OR CONTRIBUTORS  BE  LIABLE FOR  ANY DIRECT,  INDIRECT,
+ * INCIDENTAL,   SPECIAL,    EXEMPLARY,   OR   CONSEQUENTIAL   DAMAGES
+ * (INCLUDING, BUT NOT LIMITED  TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE,  DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT  LIABILITY,  OR  TORT  (INCLUDING NEGLIGENCE  OR  OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "siot/acknowledgementdecorator.h"
+#include "siot/connection.h"
+#include "siot/server.h"
+
+#include <string>
+
+namespace toolbox
+{
+namespace siot
+{
+using std::string;
+
+AcknowledgementDecorator::AcknowledgementDecorator(
+		Connection* wrapped, uint64_t max_buffer_size, bool own)
+: wrapped_(wrapped), owned_(0), max_buffer_size_(max_buffer_size)
+{
+	if (own)
+		owned_.Reset(wrapped);
+}
+
+AcknowledgementDecorator::~AcknowledgementDecorator()
+{
+}
+
+string
+AcknowledgementDecorator::Receive(size_t ignored, int flags)
+{
+	string data = wrapped_->Receive(0, flags);
+	if (data.length() > 0)
+		buffer_ += data;
+
+	if (buffer_.length() > max_buffer_size_)
+		throw ClientConnectionException("buffer size exceeded",
+				"The specified buffer size has been exceeded");
+
+	return buffer_;
+}
+
+bool
+AcknowledgementDecorator::Acknowledge(size_t bytes)
+{
+	if (bytes > buffer_.length())
+		return false;
+	buffer_ = buffer_.substr(bytes);
+	return true;
+}
+
+ssize_t
+AcknowledgementDecorator::Send(string data, int flags)
+{
+	return wrapped_->Send(data, flags);
+}
+
+string
+AcknowledgementDecorator::PeerAsText()
+{
+	return wrapped_->PeerAsText();
+}
+
+Server*
+AcknowledgementDecorator::GetServer()
+{
+	return wrapped_->GetServer();
+}
+
+bool
+AcknowledgementDecorator::IsEOF()
+{
+	return wrapped_->IsEOF() && buffer_.length() == 0;
+}
+
+uint64_t
+AcknowledgementDecorator::GetLastUse()
+{
+	return wrapped_->GetLastUse();
+}
+
+void
+AcknowledgementDecorator::SetBlocking(bool blocking)
+{
+	wrapped_->SetBlocking(blocking);
+}
+
+void
+AcknowledgementDecorator::Shutdown()
+{
+	wrapped_->Shutdown();
+}
+
+}  // namespace siot
+}  // namespace toolbox
---
+++ acknowledgementdecorator_test.cc
@@ -0,0 +1,86 @@
+/**
+ * Tests for the acknowledgement decorator implementation.
+ */
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include <toolbox/scopedptr.h>
+
+#include "siot/connection.h"
+#include "siot/acknowledgementdecorator.h"
+
+namespace toolbox
+{
+namespace siot
+{
+namespace testing
+{
+using ::testing::Return;
+using std::string;
+
+class MockConnection : public Connection
+{
+public:
+	MOCK_METHOD2(Receive, string(size_t maxlen, int flags));
+	MOCK_METHOD2(Send, ssize_t(string data, int flags));
+	MOCK_METHOD0(PeerAsText, string());
+	MOCK_METHOD0(GetServer, Server*());
+	MOCK_METHOD0(IsEOF, bool());
+	MOCK_METHOD0(GetLastUse, uint64_t());
+	MOCK_METHOD1(SetBlocking, void(bool));
+	MOCK_METHOD0(Shutdown, void());
+};
+
+class AcknowledgementDecoratorTest : public ::testing::Test
+{
+};
+
+TEST_F(AcknowledgementDecoratorTest, ReadRepeatedly)
+{
+	MockConnection* mc = new MockConnection;
+	AcknowledgementDecorator ack(mc, 512);
+	string rv;
+
+	EXPECT_CALL(*mc, Receive(0, 0))
+		.WillOnce(Return("In... "))
+		.WillOnce(Return("wait for it... "))
+		.WillOnce(Return("credible!"))
+		.WillOnce(Return(""));
+	EXPECT_CALL(*mc, IsEOF())
+		.Times(2)
+		.WillRepeatedly(Return(true));
+
+	EXPECT_EQ("In... ", ack.Receive());
+	EXPECT_EQ("In... wait for it... ", ack.Receive());
+	EXPECT_EQ("In... wait for it... credible!", ack.Receive());
+	rv = ack.Receive();
+	EXPECT_EQ("In... wait for it... credible!", rv);
+	EXPECT_FALSE(ack.IsEOF());
+
+	EXPECT_FALSE(ack.Acknowledge(rv.length() + 10));
+	EXPECT_TRUE(ack.Acknowledge(rv.length()));
+	EXPECT_TRUE(ack.IsEOF());
+}
+
+TEST_F(AcknowledgementDecoratorTest, AckLess)
+{
+	MockConnection* mc = new MockConnection;
+	AcknowledgementDecorator ack(mc, 512);
+
+	EXPECT_CALL(*mc, Receive(0, 0))
+		.WillOnce(Return("0123456789"))
+		.WillOnce(Return(""));
+
+	EXPECT_EQ("0123456789", ack.Receive());
+	EXPECT_TRUE(ack.Acknowledge(5));
+	EXPECT_EQ("56789", ack.Receive());
+}
+
+}  // namespace testing
+}  // namespace siot
+}  // namespace toolbox
